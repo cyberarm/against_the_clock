a8ec014397ff8474c6d8b4a046cb5e6f8e719793
Cyberarm <matthewlikesrobots@gmail.com>
2017-01-04 21:39:41 -0600

    Simplied things

--- against_the_clock.rb
+++ against_the_clock.rb
@@ -1,74 +1,84 @@
 require "gosu"
 # require_relative "lib/menu"
 require_relative "lib/text"
-Modifier = Struct.new(:value, :dangerous_amount, :fatal_amount, :decrease_per_second, :time_at_last_change)
+Modifier = Struct.new(:name, :value, :dangerous_amount, :fatal_amount, :decrease_per_second, :time_at_last_change, :bar_color, :death_messages)
 class Window < Gosu::Window
   def initialize
     $window = self
     super(800, 600, false)
     @dead  = false
     @time  = rand(3..7)*60*60*24
-    @speed = rand(10..25)
+    @speed = 10000#rand(10..25)
     @dt    = (Gosu.milliseconds/1000.0)
     @clock = Game::Text.new(seconds_to_time(@time), x: 0, y: 36, color: Gosu::Color::WHITE, size: 24)
+    @cause_of_death = Game::Text.new("", x: 36, y: 300, color: Gosu::Color::WHITE, size: 24)
     @font  = Gosu::Font.new($window, Gosu.default_font_name, 30)
     @clock.x    = @clock.width/4
     @max_value = 100.0
     @spacer    = 120
+    @modifiers = []
 
     @hunger = Modifier.new
+    @hunger.name  = "Food"
     @hunger.value = 100.0
     @hunger.dangerous_amount = 25.0
     @hunger.fatal_amount = 0.0
     @hunger.decrease_per_second = 0.01
     @hunger.time_at_last_change = (Gosu.milliseconds/1000.0)*@speed
+    @hunger.bar_color = Gosu::Color.rgb(100,0,30)
+    @hunger.death_messages = ["Got lost in a desert without food.", "Lost your ration pack."]
 
     @hydration = Modifier.new
-    @hydration.value = 70.0
-    @hydration.dangerous_amount = 100.0
+    @hydration.name  = "Hydration"
+    @hydration.value = 100.0
+    @hydration.dangerous_amount = 60.0
     @hydration.fatal_amount = 0.0
     @hydration.decrease_per_second = 0.05
     @hydration.time_at_last_change = (Gosu.milliseconds/1000.0)*@speed
+    @hydration.bar_color = Gosu::Color::BLUE
+    @hydration.death_messages = ["Got lost in a desert without water.", "Dried up like a raisin."]
 
     @happiness = Modifier.new
+    @happiness.name  = "Happiness"
     @happiness.value = 100.0
     @happiness.dangerous_amount = 35.0
     @happiness.fatal_amount = 0.0
     @happiness.decrease_per_second = 0.03
     @happiness.time_at_last_change = (Gosu.milliseconds/1000.0)*@speed
+    @happiness.bar_color = Gosu::Color.rgb(0,100,0)
+    @happiness.death_messages = ["Got lost in a daydream while driving.", "Lost your your will to live."]
+
+    add_modifier(@hunger)
+    add_modifier(@hydration)
+    add_modifier(@happiness)
 
     self.caption = "Against the Clock - FPS:#{Gosu.fps}"
   end
 
   def line(x, y, color = Gosu::Color::BLACK)
-    fill_rect(x, y, 2, 25, color)
+    fill_rect(x, y, 2, 25, color, 10)
+  end
+
+  def add_modifier(modifier)
+    @modifiers << modifier
   end
 
   def draw
     @clock.draw
+    y = 100
+    @modifiers.each do |modifier|
+      fill_rect(@spacer, y, @max_value*5, 25, Gosu::Color::GRAY)
+      fill_rect(@spacer, y, ((modifier.value/@max_value)*100.0)*5, 25, modifier.bar_color) unless modifier.value < 0
+      line(((modifier.dangerous_amount/@max_value)*100.0)*5+@spacer, y)
+      @font.draw("#{modifier.name}", @spacer+20, y, 1)
+      @font.draw("DEATH", 20, y, 1)
+      @font.draw("LIFE", 500+@spacer+16, y, 1)
 
-    fill_rect(@spacer, 100, @max_value*5, 25, Gosu::Color::GRAY)
-    fill_rect(@spacer, 100, ((@hunger.value/@max_value)*100.0)*5, 25, Gosu::Color.rgb(100,0,30)) unless @hunger.value < 0
-    line(((@hunger.dangerous_amount/@max_value)*100.0)*5+@spacer, 100)
-    @font.draw("Food", @spacer+20, 100, 1)
-    @font.draw("DEATH", 20, 100, 1)
-    @font.draw("LIFE", 500+@spacer+36, 100, 1)
+      y+=50
+    end
 
-    fill_rect(@spacer, 150, @max_value*5, 25, Gosu::Color::GRAY)
-    fill_rect(@spacer, 150, ((@hydration.value/@max_value)*100.0)*5, 25, Gosu::Color::BLUE) unless @hydration.value < 0
-    line(((@hydration.dangerous_amount/@max_value)*100.0)*5+@spacer, 150)
-    @font.draw("Hydration", @spacer+20, 150, 1)
-    @font.draw("DEATH", 20, 150, 1)
-    @font.draw("LIFE", 500+@spacer+36, 150, 1)
-
-    fill_rect(@spacer, 200, @max_value*5, 25, Gosu::Color::GRAY)
-    fill_rect(@spacer, 200, ((@happiness.value/@max_value)*100.0)*5, 25, Gosu::Color.rgb(0,100,0)) unless @happiness.value < 0
-    line(((@happiness.dangerous_amount/@max_value)*100.0)*5+@spacer, 200)
-    @font.draw("Happiness", @spacer+20, 200, 1)
-    @font.draw("DEATH", 20, 200, 1)
-    @font.draw("LIFE", 500+@spacer+36, 200, 1)
     if @dead
-
+      @cause_of_death.draw
     end
   end
 
@@ -77,15 +87,13 @@ class Window < Gosu::Window
     @dt = (Gosu.milliseconds/1000.0)
     self.caption = "Against the Clock - FPS:#{Gosu.fps} - @speed: #{@speed} - #{((@happiness.value/@max_value)*100.0)*5}"
 
-    check_modifier(@hunger)
+    unless @dead
+      @modifiers.each {|m| check_modifier(m)}
+    end
 
-    check_modifier(@hydration)
-
-    check_modifier(@happiness)
-
-    if @time <= 0
+    if @time <= 0 && !@dead
       @dead = true
-      @clock.text = "You have died."
+      @clock.text = ""+seconds_to_time(@time)
     end
   end
 
@@ -97,7 +105,14 @@ class Window < Gosu::Window
     end
 
     if modifier.value <= modifier.dangerous_amount
-      @time -= modifier.decrease_per_second*@speed
+      @time -= modifier.decrease_per_second
+    end
+
+    if modifier.value <= modifier.fatal_amount && !@dead
+      @time = 0
+      @dead = true
+      n = rand(0..modifier.death_messages.count-1)
+      @cause_of_death.text = "#{modifier.death_messages[n]}"
     end
   end
 
